b223953de5bdf5251b3bf7bca2880ef762727729
Solomatin Mikhail <mi.solomatin@ase-ec.ru>
2022-04-13 07:57:19 +0300

    правки по стилям

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,19 @@
 import Container from '../Container'
 import logo from '../../assets/logo.png'
 
-import style from './Header.module.css'
+import s from './Header.module.css'
 
 const MENU = ['Menu 1', 'Menu 2', 'Menu 3', 'Menu 4'];
 
 const Header = () => {
   return (
-    <header className={style.root}>
-      <div className={style.header}>
-        <Container className={style.container}>
-          <div className={style.image}>
-            <img src={logo} alt="logo" className={style.logo}/>
+    <header className={s.root}>
+      <div className={s.header}>
+        <Container className={s.container}>
+          <div className={s.logo}>
+            <img src={logo} alt="logo" className={s.img}/>
           </div>
-          <ul className={style.nav}>
+          <ul className={s.nav}>
             {MENU.map(item => {
               return (
                 <li>
